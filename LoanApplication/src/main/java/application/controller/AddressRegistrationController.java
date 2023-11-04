16211d1957577b83f8b92c401b3d2b8d13a67777
Jakub.other.usr <jakub.other.usr@Codiblys-MacBook-Pro.local>
2023-11-04 10:45:37 +0100

    L02 adding validation for vields + controllers to map person and address data

---
+++ LoanApplication/src/main/java/application/controller/AddressRegistrationController.java
@@ -0,0 +1,30 @@
+package application.controller;
+
+
+import application.modelDto.Address;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AddressRegistrationController {
+
+    @PostMapping("/submit_address")
+    public ResponseEntity<String> collectAddressForm(Address addressForm) {
+
+        if(!isAddressFormDataValid(addressForm)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Provided address data is incorrect");
+        }
+        System.out.println("Address form data ");
+        System.out.println("Post code " + addressForm.getPostalCode());
+        System.out.println("Address form " + addressForm.getTown());
+        System.out.println("Street name " + addressForm.getStreetName());
+        System.out.println("Address form " + addressForm.getEmail());
+        return ResponseEntity.status(HttpStatus.OK).body("Address data saved successfully");
+    }
+
+    private boolean isAddressFormDataValid(Address addressForm) {
+        return true;
+    }
+}
---
+++ LoanApplication/src/main/java/application/controller/PersonRegistrationController.java
@@ -0,0 +1,31 @@
+package application.controller;
+
+
+import application.modelDto.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class PersonRegistrationController {
+
+    @PostMapping("/submit_registration")
+    public ResponseEntity<String> collectPersonForm(@ModelAttribute("personForm") Person personForm) {
+        if (!isAddressDataValid(personForm)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Provided data is invalid");
+        }
+        System.out.println("First Name: " + personForm.getFirstName());
+        System.out.println("Last Name: " + personForm.getLastName());
+        System.out.println("Birth Date: " + personForm.getBirthDate());
+        System.out.println("Phone Number: " + personForm.getPhoneNumber());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Registration data saved successfully");
+    }
+
+    private boolean isAddressDataValid(Person personForm) {
+        return true;
+    }
+}
